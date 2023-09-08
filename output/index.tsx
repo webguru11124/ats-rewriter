70b8bfae4e00235e516ee41d63033a5ac1a9cc2e
webguru11124 <lucas.eriksson.pro@gmail.com>
2023-09-09 05:35:03 +0900

    fix: working with CLI

--- output/index.tsx
+++ output/index.tsx
@@ -1,5 +1,5 @@
 // EntityComponentList.tsx
-export const EntityComponent = (props) => {
-    [data] = useEntityQuery()
-    return data.map(d => <EntityComponent data={d} />
+export const RenamedComponent = (props) => {
+    [data] = useRenamedQuery()
+    return data.map(d => <RenamedComponent data={d} />
  }
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-import { Project } from 'ts-morph';
+import { Project, SourceFile } from 'ts-morph';
 import * as path from 'path';
 import * as fs from 'fs';
 
 function transformFile(filePath: string, entityName: string, outputDirectory: string) {
   const project = new Project();
   const sourceFile = project.addSourceFileAtPath(filePath);
-  console.log(sourceFile);
-  // Function to rename nodes containing 'Entity' or 'entities'
+
+  // Clone the source file's content
+  const sourceFileText = sourceFile.getFullText();
+
+  // Create a new source file with the same content
+  const sourceFileCopy = project.createSourceFile('temp.ts', sourceFileText, { overwrite: true });
+
+  // Function to rename nodes containing 'Entity' or 'entities' in the copy
   function renameNodes(node: any) {
-    console.log(node.getKindName(), node.getText())
-    if (node.getKindName() === 'FunctionDeclaration' || node.getKindName() === 'FunctionExpression') {
+    if (node.getKindName() === 'FunctionDeclaration' || node.getKindName() === 'FunctionExpression' || node.getKindName() === 'Identifier') {
       const text = node.getText();
       if (text.includes('Entity') || text.includes('entities')) {
-        node.rename(`Renamed${entityName}`);
+        const updatedText = node.getText().replace(/Entity|entities/g, entityName);
+        node.replaceWithText(`${updatedText}`);
       }
-    } else if (node.getKindName() === 'StringLiteral') {
-      const updatedText = node.getText().replace(/Entity|entities/g, entityName);
-      node.replaceWithText(`"${updatedText}"`);
     }
 
     node.getChildren().forEach(renameNodes);
   }
 
-  // Rename nodes in the AST
-  renameNodes(sourceFile);
+  // Rename nodes in the copy
+  renameNodes(sourceFileCopy);
 
-  // Save the modified source file
-  sourceFile.saveSync();
+  // Save the modified copy to the output directory
   const outputFilePath = path.join(outputDirectory, path.basename(filePath).replace('Entity', entityName));
-  fs.copyFileSync(filePath, outputFilePath);
+  fs.writeFileSync(outputFilePath, sourceFileCopy.getFullText());
 }
 
 const inputDirectory = 'input';
